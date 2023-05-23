63b6ab28d95c822ba6396977d2b6f71b0b0c36d9
abdulkareemAbunabhan <zman17881@gmail.com>
2023-05-23 06:40:27 +0000

    done the lab

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -1,17 +1,23 @@
 from http.server import BaseHTTPRequestHandler
 import requests 
+from urllib import parse
 class handler(BaseHTTPRequestHandler):
-    base_url=f"http://api.geonames.org/searchJSON?q={country_name}&featureCode=PPLC&maxRows=1&username=abdulkareem98"
-    response = requests.get(base_url, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        if "geonames" in data and len(data["geonames"]) > 0:
-            capital = data["geonames"][0]["capital"]
-            capital
     
-    # return None    def do_GET(self):
-    #     self.send_response(200)
-    #     self.send_header('Content-type','text/plain')
-    #     self.end_headers()
-    #     self.wfile.write('Hello, world!'.encode('utf-8'))
-    #     return
+    def do_GET(self):
+        path= self.path
+        url_components=parse.urlsplit(path)
+        query_string_list=parse.parse_qsl(url_components)
+        params=dict(query_string_list)
+        country_name=params.get("country_name")
+        if(country_name):
+            url=f"http://api.geonames.org/searchJSON?q={country_name}&featureCode=PPLC&maxRows=1&username=abdulkareem98"
+            resp=requests.get(url)
+            data=resp.json()
+            capital_name=data["geonames"][0]["name"]
+        else:
+            capital_name="Nothing"
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write(capital_name.encode('utf-8'))
+        return
--- api/country_finder.py
+++ api/country_finder.py
@@ -7,8 +7,16 @@ class handler(BaseHTTPRequestHandler):
         url_components=parse.urlsplit(path)
         query_string_list=parse.parse_qsl(url_components)
         params=dict(query_string_list)
+        capital_name=params.get("capital_name")
+        if(capital_name):
+            url=f"http://api.geonames.org/searchJSON?q={capital_name}&featureCode=PPLC&maxRows=1&username=abdulkareem98"
+            resp=requests.get(url)
+            data=resp.json()
+            country_name=data["geonames"][0]["countryName"]
+        else:
+            country_name="Nothing"
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write(params.encode('utf-8'))
+        self.wfile.write(country_name.encode('utf-8'))
         return
