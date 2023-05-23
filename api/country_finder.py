e8baeabfadef6764b9c1123df4f24c7f3ed6a794
abdulkareemAbunabhan <zman17881@gmail.com>
2023-05-23 06:20:16 +0000

    feat: country finder

--- api/country_finder.py
+++ api/country_finder.py
@@ -1,11 +1,14 @@
 from http.server import BaseHTTPRequestHandler
+from urllib import parse
 import requests 
 class handler(BaseHTTPRequestHandler):
- 
-    request=requests.get(f"http://api.geonames.org/searchJSON?q={capital_city_name}&featureCode=PPLC&maxRows=1&username=abdulkareem98")
-    def do_GET(self):
+     def do_GET(self):
+        path= self.path
+        url_components=parse.urlsplit(path)
+        query_string_list=parse.parse_qsl(url_components)
+        params=dict(query_string_list)
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write(params.encode('utf-8'))
         return
